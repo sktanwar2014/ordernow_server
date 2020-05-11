1bf61074a0cf6881cbb339ea0c7283dae6d4fa77
mpurohit88 <root@vps.a1abilities.co.nz>
2020-05-11 13:50:39 +0530

    fix to upload issue

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const session = require('express-session');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+//app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb', extend: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true}));
 
 app.use(cookieParser());
 app.use(session({
